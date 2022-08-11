4616dd7b7abaa414ac4bfaf1c8612a350cb6459e
Saroj Rai <raisaroj360@gmail.com>
2022-08-11 15:12:47 +0545

    adsense script added

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../config/theme.js';
@@ -5,6 +6,13 @@ import Fonts from '../config/fonts.js';
 
 function MyApp({ Component, pageProps }) {
   return (<ChakraProvider theme={theme}>
+            <Script
+              id="adsense-id"
+              async="true"
+              strategy="afterInteractive"
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4040965046942345"
+              crossorigin="anonymous"
+              onError={(e) => { console.error('Script failed to load', e); }}/>
             <Fonts/>
             <Component {...pageProps} />
           </ChakraProvider>);
